02eda529857246a1f5a199d4f5695b352771b9d3
Jeronimo Nuñez <jeronimonunez@gmail.com>
2024-07-23 16:57:51 -0300

    first commit

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
 
-export default function SideNav() {
+const SideNav = () => {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -26,4 +26,6 @@ export default function SideNav() {
       </div>
     </div>
   );
-}
+};
+
+export default SideNav;
